df0221d5a78835e8a40cd1ad34db748b1faf58ad
Tim Ottinger <tottinge@gmail.com>
2016-07-24 20:56:05 -0500

    Initial

---
+++ source/dns_lookup.py
@@ -0,0 +1,19 @@
+import string
+import socket
+
+def dnslookup(self, str):
+        """ Perform DNS lookup on str.  If first character of digit is numeric,
+            assume that str contains an IP address.  Otherwise, assume that str
+            contains a hostname."""
+        if str == '': str = ' '
+        if str[0] in string.digits:
+            try:
+                value = socket.gethostbyaddr(str)[0]
+            except:
+                value = 'Lookup failed'
+        else:
+            try:
+                value = socket.gethostbyname(str)
+            except:
+                value = 'Lookup failed'
+        return value
---
+++ tests/test_dnslookup.py
@@ -0,0 +1,6 @@
+import unittest
+
+class testDnsLookup(unittest.TestCase):
+    def test_whatever(self):
+        self.fail("great!")
+
